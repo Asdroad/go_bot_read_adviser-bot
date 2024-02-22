887046f3c50fbc5d61a2c64cd78a7862cd8890a1
griseldy <khairov.tima@bk.ru>
2024-02-22 22:27:13 +0300

    first main

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+
+	t := mustToken()
+	// token= flags.get.token()
+
+	//tgClient= telegram.New(token)
+
+	//fetcher= fetcher.New(tgClient)
+
+	//processor= processor.New(tgClient)
+
+	//consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	token := flag.String("token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token in not specified")
+	}
+
+	return *token
+}
